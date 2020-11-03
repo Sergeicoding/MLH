ad8c9f5e8d413b6cf1d5702cbee756c6d265f2e5
Sergei Ioannisyan <sergeiioannisyan@MacBook-Pro.local>
2020-11-02 21:19:57 -0500

    test1

--- test/smoke/labels.js
+++ test/smoke/labels.js
@@ -37,7 +37,7 @@ describe('My Little Hero', function () {
     });
     describe('Labels are correct', function () {
 
-        it('TC-006 Label for name = 1. What is your HERO\'s name?', function () {
+        it('TC-006 Label for name = 1. What is your Hero\'s name?', function () {
             const text = $$(sel.label)[0].getAttribute('title');
             expect(text).toEqual(exp.labelName);
         });
@@ -47,15 +47,15 @@ describe('My Little Hero', function () {
             expect(text).toEqual(exp.labelGender);
         });
 
-        /*it('TC-008 Label for age', function () {
-            const label = $$('.ant-form-item-required')[2].isDisplayed();
-            expect(label).toEqual(true);
+        it('TC-008 Label for age = 3. How old is your hero?', function () {
+            const text = $$(sel.label)[2].getAttribute('title');
+            expect(text).toEqual(exp.labelAge);
         });
 
-        it('TC-009 Label for story', function () {
-            const label = $$('.ant-form-item-required')[3].isDisplayed();
-            expect(label).toEqual(true);
-        });*/
+        it('TC-009 Label for story = 4. What type of story would you like to read?', function () {
+            const text = $$(sel.label)[3].getAttribute('title');
+            expect(text).toEqual(exp.labelStory);
+        });
 
     });
 
